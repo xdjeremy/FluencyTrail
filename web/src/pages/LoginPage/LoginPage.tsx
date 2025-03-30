8aa6020d2c6bdcbe870284612af4e8526c46874c
xdjeremy <jeremy.fran@icloud.com>
2025-03-30 19:03:40 +0800

    Refactor LoginPage component to use email instead of username; update form handling and remove unused code

--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from '@redwoodjs/forms';
+import { SubmitHandler, useForm } from '@redwoodjs/forms';
 import { Link, navigate, routes } from '@redwoodjs/router';
 import { Metadata } from '@redwoodjs/web';
 import { Book, Loader2 } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
+import { toast } from 'sonner';
 import { useAuth } from 'src/auth';
 import { Button } from 'src/components/ui/button';
 import {
@@ -16,7 +17,6 @@ import {
 } from 'src/components/ui/form';
 import { Input } from 'src/components/ui/input';
 import { LoginSchema, LoginSchemaType } from './LoginSchema';
-import { toast } from 'sonner';
 
 const LoginPage = () => {
   const { isAuthenticated, logIn } = useAuth();
@@ -36,12 +36,12 @@ const LoginPage = () => {
     }
   }, [isAuthenticated]);
 
-  const usernameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
-    usernameRef.current?.focus();
+    emailRef.current?.focus();
   }, []);
 
-  const onSubmit = async (data: Record<string, string>) => {
+  const onSubmit: SubmitHandler<LoginSchemaType> = async data => {
     setIsLoading(true);
     const response = await logIn({
       username: data.email,
@@ -100,6 +100,7 @@ const LoginPage = () => {
                         disabled={isLoading}
                         {...field}
                         aria-disabled={isLoading}
+                        ref={emailRef}
                       />
                     </FormControl>
                     <FormMessage />
@@ -225,85 +226,6 @@ const LoginPage = () => {
           </div>
         </Form>
       </div>
-      {/* <main className="rw-main">
-        <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-        <div className="rw-scaffold rw-login-container">
-          <div className="rw-segment">
-            <header className="rw-segment-header">
-              <h2 className="rw-heading rw-heading-secondary">Login</h2>
-            </header>
-
-            <div className="rw-segment-main">
-              <div className="rw-form-wrapper">
-                <Form onSubmit={onSubmit} className="rw-form-wrapper">
-                  <Label
-                    name="username"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Username
-                  </Label>
-                  <TextField
-                    name="username"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    ref={usernameRef}
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Username is required',
-                      },
-                    }}
-                  />
-
-                  <FieldError name="username" className="rw-field-error" />
-
-                  <Label
-                    name="password"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Password
-                  </Label>
-                  <PasswordField
-                    name="password"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    autoComplete="current-password"
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Password is required',
-                      },
-                    }}
-                  />
-
-                  <div className="rw-forgot-link">
-                    <Link
-                      to={routes.forgotPassword()}
-                      className="rw-forgot-link"
-                    >
-                      Forgot Password?
-                    </Link>
-                  </div>
-
-                  <FieldError name="password" className="rw-field-error" />
-
-                  <div className="rw-button-group">
-                    <Submit className="rw-button rw-button-blue">Login</Submit>
-                  </div>
-                </Form>
-              </div>
-            </div>
-          </div>
-          <div className="rw-login-link">
-            <span>Don&apos;t have an account?</span>{' '}
-            <Link to={routes.signup()} className="rw-link">
-              Sign up!
-            </Link>
-          </div>
-        </div>
-      </main> */}
     </>
   );
 };
